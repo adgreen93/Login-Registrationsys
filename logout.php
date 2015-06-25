cddc17f46d0c9c79b4f79fd99de58619f31bb260
Al Green <adgreen93@gmail.com>
2015-06-25 09:07:54 -0400

    Create logout.php
    Logout page. Session destroy keeps user from just going back to access page.

---
+++ logout.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE);
+session_start();
+session_destroy();
+
+?>
+
+
+
+<!DOCTYPE html>
+
+
+<html>
+
+<head>
+
+
+</head>
+
+<body>
+<a href="index.php"> Click here to return to main page. </a>
+</body>
+
+</html>
